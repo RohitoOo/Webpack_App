7f3ae08b9f16f180963b35ac802fa582d49d5f1f
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-07-18 14:53:39 +0530

    Require Jquery Test

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert( require("./people.js") ) 
+
+let theBrothers = require('./people.js');
+let $ = require('jquery');
+
+$('body').append('<h1>'+ theBrothers[0].name+'</h1>');
+
+console.log(theBrothers);
