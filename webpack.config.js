143c871cf0a38766e5a28fd7c0d020fa70b26377
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-07-18 23:42:46 +0530

    Added Babel for Wider Compatibility of Browsers

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,20 @@ module : {
         { loader: "css-loader" }
       ]
 
+    },
+
+    {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+
     }
     ]
-  }
+
+}
 }
