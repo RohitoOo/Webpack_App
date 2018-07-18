ea6bff8a99f687ed11f1528b5f56824bd0ea9a04
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-07-18 16:17:59 +0530

    Added Css Loaders

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 
 let theBrothers = require('./people.js');
 let $ = require('jquery');
+require('!style-loader!css-loader!./style.css')
 
-$('body').append('<h1>'+ theBrothers[0].name+'</h1>');
+$.each(theBrothers, (key, value) => {
+
+  $('body').append( '<h1>'+ (key+1) + ". " + theBrothers[key].name+'</h1>');
+
+})
 
 console.log(theBrothers);
